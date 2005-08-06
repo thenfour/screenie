8052ae162eb57f7f3396d1678af5da6e6f305a35
carl <carl>
2005-08-06 23:08:03 +0000

    - version numbers better in veredit (veredit.exe's version that is) - honing official_build, but not done yet.

--- official_build.cmd
+++ official_build.cmd
@@ -8,43 +8,36 @@ echo Confirming arguments / variables...
   set serial=%1
   set registrant=%2
 
-echo Checking for tools...
-  where devenv.exe >nul
-  if %errorlevel% gtr 0 goto DevenvNotFound
+echo Checking for tools / files...
   where makensis.exe >nul
   if %errorlevel% gtr 0 goto NsisNotFound
-
-echo Checking for local changes...
-  rem Create a temp file (SubWCRev needs to process files, but we don't need them now... so just make throwaways)
-  echo. >"%temp%\tempver.in"
-  "%svnroot%\tools\SubWCRev.exe" "%svnroot%" "%temp%\tempver.out" "%temp%\tempver.in" -n >nul
-  echo UNCOMMENT THIS
-  rem if %errorlevel% gtr 0 goto LocalMods
+  if not exist "%svnroot%\bin-release\screenie.exe" goto NotBuilt
 
 echo Generating output directory...
   md "%svnroot%\distro" 2>nul
   set outdir=%svnroot%\distro\%serial%
   md "%outdir%" 2>nul
 
-echo Setting up version info...
-  echo NOT DONE
-
-echo Building...
-  devenv "%svnroot%\source\client\screenie-2k3.vcproj" /rebuild release /out "%outdir%\buildlog.txt"
-  if %errorlevel% gtr 0 goto BuildError
-
 echo Copying program binaries...
   copy "%svnroot%\bin-release\screenie.exe" "%outdir%" >nul
 
+echo Generating version information for "%svnroot%\source\client"
+  "%svnroot%\tools\SubWCRev.exe" "%svnroot%\source\client" "%svnroot%\distro\ver_in.xml" "%outdir%\ver_out.xml" -n
+  if %errorlevel% gtr 0 goto LocalMods
+
+echo Setting up version info...
+  "%svnroot%\tools\veredit.exe" "%outdir%\screenie.exe" /xml "%outdir%\ver_out.xml" /string RegisteredTo="%registrant%"
+  if %errorlevel% gtr 0 goto VereditError
+
 echo Watermarking...
-  cd /d "%outdir%"
-  "%svnroot%\tools\PEWaterMark.exe" "%outdir%\screenie.exe" /mark=%serial%
+  rem cd /d "%outdir%"
+  rem "%svnroot%\tools\PEWaterMark.exe" "%outdir%\screenie.exe" /mark=%serial%
 
 echo Building Installer...
-  makensis.exe "%svnroot%\source\installer\screenie.nsi" /O"%outdir%\nsislog.txt" /Dregistrant="%registrant%" /Dserial="%serial%"
+  rem makensis.exe "%svnroot%\source\installer\screenie.nsi" /O"%outdir%\nsislog.txt" /Dregistrant="%registrant%" /Dserial="%serial%"
 
 echo Copying final installer...
-  copy "%svnroot%\bin-installer\ScreenieSetup.exe" "%outdir%" >nul
+  rem copy "%svnroot%\bin-installer\ScreenieSetup.exe" "%outdir%" >nul
 
 echo All done!  SUCCESS.
   cd /d %svnroot%
@@ -58,16 +51,16 @@ echo * official_build.cmd [serial] [registrant]
 echo * Also, You must run this from the svn.screenie\root directory (the directory that contains this file)
 goto End
 
-:DevenvNotFound
-echo !!Devenv.exe was not found in your path.  Stopping.
-goto End
-
 :NsisNotFound
 echo !!makensis.exe was not found in your path.  Make sure you have nsis installed, and it's location is in your path environment variable. Stopping.
 goto End
 
-:BuildError
-echo !!Build errors were found.  Stopping.
+:VereditError
+echo !!veredit returned an error.  Stopping.
+goto End
+
+:NotBuilt
+echo !!screenie.exe has not been built.  Stopping.
 goto End
 
 :LocalMods
