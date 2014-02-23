9d4709696f7be204d11442045ad1d316b2325c66
rogerclark <rogerclark@gmail.com>
2014-02-23 01:56:51 -0800

    add a dummy version.h

---
+++ source/client/source/version.h
@@ -0,0 +1,12 @@
+#define FILEVER        2,0,0,0
+#define PRODUCTVER     FILEVER
+#define STRFILEVER     "2, 0, 0, 0\0"
+#define STRPRODUCTVER  "2, 0, 0, 0\0"
+#define STRREGISTRANT  "[development]\0"
+
+#define TSVN_VERMAJOR             2
+#define TSVN_VERMINOR             0
+#define TSVN_VERMICRO             0
+#define TSVN_VERBUILD			  0
+
+
